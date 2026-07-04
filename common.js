361dbe85dd2841deed2d3e7cefe4b7d38b9e4fa2
JESN-CAV <jhys.sais@gmail.com>
2026-07-04 14:17:50 +0200

    test6

--- common.js
+++ common.js
@@ -675,10 +675,10 @@ if (config.type === 'colorLast12Months') {
                 point:{
                     events:{
                         click:function(){
-                            alert("2");/*
-                            alert("Click sur " + this.category);
+                            //alert("2");
+                            //alert("Click sur " + this.category);
                             const index = this.index;
-                            populateTable(rowsForCategory[index]);*/
+                            populateTable(rowsForCategory[index]);
                         }
                     }
                 }
