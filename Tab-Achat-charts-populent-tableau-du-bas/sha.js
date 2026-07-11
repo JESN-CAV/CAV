a17dc7c0e30abf192bffd82189c776b4e8818493
JESN-CAV <JESN-CAV@users.noreply.github.com>
2026-07-11 10:42:46 +0000

    deploy: 0f35163972dbf6c9267aeac0d738e75a7e98458e

---
+++ Tab-Achat-charts-populent-tableau-du-bas/sha.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', async () => {
+
+  try {
+
+    const response = await fetch('./version.json', {
+      cache: 'no-store'
+    });
+
+    if (!response.ok) {
+      throw new Error('version.json introuvable');
+    }
+
+    const data = await response.json();
+
+    const versionElement = document.getElementById('version');
+
+    if (versionElement) {
+      versionElement.textContent =data.sha +" — "+ data.when;
+    }
+
+  } catch (error) {
+
+    alert('Erreur chargement version :', error);
+
+    const versionElement = document.getElementById('version');
+
+    if (versionElement) {
+      versionElement.textContent = 'dev';
+    }
+  }
+
+});
+
